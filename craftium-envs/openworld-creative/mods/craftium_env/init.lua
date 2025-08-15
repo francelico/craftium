4d8b7df8ed7a5f4dfb7816367fb22e72a445ae8c
Samuel Garcin <garcin.samuel@gmail.com>
2025-08-15 15:24:51 +0100

    fixed timeofday logic

--- craftium-envs/openworld-creative/mods/craftium_env/init.lua
+++ craftium-envs/openworld-creative/mods/craftium_env/init.lua
@@ -13,8 +13,7 @@ voxel_radius = {
 -- names of the items included in the initial inventory
 init_inv = { "mcl_core:stone" }
 
--- timeofday_step = 1 / 5000 -- day/night cycle lasts 5000 steps
--- timeofday = tonumber(minetest.settings:get("world_start_time"))/24000
+timeofday = tonumber(minetest.settings:get("world_start_time"))/24000
 
 -- executed when the player joins the game
 minetest.register_on_joinplayer(function(player, _last_login)
@@ -29,6 +28,8 @@ minetest.register_on_joinplayer(function(player, _last_login)
 		inv:add_item("main", init_inv[i])
 	end
 
+	minetest.set_timeofday(timeofday)
+
 end)
 
 -- turn on the termination flag if the agent dies
@@ -38,12 +39,6 @@ end)
 
 -- make game's time match with learning timesteps
 minetest.register_globalstep(function(dtime)
-
--- 	if timeofday > 1.0 then
--- 		timeofday = 0.0
--- 	end
--- 	minetest.set_timeofday(timeofday)
--- 	timeofday = timeofday + timeofday_step
 
 	local player = minetest.get_connected_players()[1]
 
